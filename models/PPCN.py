1c82d108540b57258abb5bf2b8392e0c43ba060d
akorol98 <akorol98.17@gmail.com>
2020-05-11 00:25:14 +0200

    last changes

--- models/PPCN.py
+++ models/PPCN.py
@@ -13,43 +13,43 @@ class PlantPathologyClassifier(nn.Module):
             nn.Conv2d(3, 16, 4, 1, 1, bias=False),
             nn.BatchNorm2d(16),
             nn.ReLU(inplace=True),
-        
+
             nn.Conv2d(16, 16, 4, 1, 1, bias=False),
             nn.BatchNorm2d(16),
             nn.LeakyReLU(0.2, inplace=True),
             nn.MaxPool2d(2, 2, 1),
-            
+
             nn.Conv2d(16, 32, 4, 1, 1, bias=False),
             nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
-            
+
             nn.Conv2d(32, 32, 4, 1, 1, bias=False),
             nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
-            
+
             nn.Conv2d(32, 64, 4, 1, 1, bias=False),
             nn.BatchNorm2d(64),
             nn.LeakyReLU(0.2, inplace=True),
             nn.MaxPool2d(2, 2, 1),
-            
+
             nn.Conv2d(64, 64, 4, 1, 1, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
-            
+
             nn.Conv2d(64, 128, 4, 1, 1, bias=False),
             nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
-            
+
             nn.Conv2d(128, 128, 4, 1, 1, bias=False),
             nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2, inplace=True),
             nn.MaxPool2d(2, 2, 1),
-            
-            
+
+
             nn.Conv2d(128, 256, 4, 1, 1, bias=False),
             nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
-            
+
             nn.Conv2d(256, 256, 4, 1, 1, bias=False),
             nn.BatchNorm2d(256),
             nn.LeakyReLU(0.2, inplace=True),
@@ -60,29 +60,29 @@ class PlantPathologyClassifier(nn.Module):
 
         # out shape [ 256, 1, 1, 1 ] from convolutional going into fully conected classifier
         self.fc = nn.Sequential(
-            
+
             nn.Linear(12544, 4096),
             nn.LeakyReLU(),
             nn.Dropout(0.5),
-            
+
             nn.Linear(4096, 1000),
             nn.LeakyReLU(),
             nn.Dropout(0.5),
-            
+
             nn.Linear(1000, 100),
             nn.LeakyReLU(),
             nn.Dropout(0.5),
-            
+
             nn.Linear(100, 4)
         )
 
     def forward(self, image):
-        
+
         conv_out = self.main_conv(image).view(-1, 12544)
         fc_input = conv_out
-        
+
         return self.fc(fc_input)
-    
+
 def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
